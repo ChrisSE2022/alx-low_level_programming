dfcdeedc311f3e0cd5105f2876d6d3bc697dd7ac
chrisSE2022 <mwanikichris90@gmail.com>
2022-07-05 05:24:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{ 
+	char x;	
+    for(char x = 'a'; x <= 'z'; x++)
+    {
+	_putchar('x');
+	_putchar('\n');
+    }
+    return 0;
+}
