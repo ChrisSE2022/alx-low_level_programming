330b55ccaf912a9288ddd6c5662763abc27bdbbe
chrisSE2022 <mwanikichris90@gmail.com>
2022-07-05 11:01:49 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
  * _islower - Shows 1 if the input is a
+ * @c: character to be displayed
  * lowercase character. Another cases, show 0
  *
  * Return: 1 for c lowercase character. 0 for the rest.
