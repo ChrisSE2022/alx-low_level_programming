660cbc130da5f5979505b19cf25e90222cc92cb7
chrisSE2022 <mwanikichris90@gmail.com>
2022-07-06 11:21:43 -0700

    0 > 972

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,21 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+
+
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
+int main(void);
+int convert_day(int month, int day);
+
+
+
+#endif /* MAIN_H */
+
 
 
