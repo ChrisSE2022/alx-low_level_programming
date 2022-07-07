a59fdd80d05943a367e94e01d9571bc0afa9b17f
chrisSE2022 <mwanikichris90@gmail.com>
2022-07-07 05:45:10 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,12 @@ void more_numbers(void)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			if (a >= 10)
-				_putchar(a / 10 + '0');
-				_putchar(a % 10 + '0');
+		if (a > 9) 
+			{			
+				_putchar(1 + '0');
+			}
+			_putchar(a % 10 + '0');
+		
 		}
 	_putchar('\n');
 	}
