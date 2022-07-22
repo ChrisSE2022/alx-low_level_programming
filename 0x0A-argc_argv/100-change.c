37615bf035ac82a76577ea63038b76c1acc84253
chrisSE2022 <mwanikichris90@gmail.com>
2022-07-22 08:25:12 -0700

    a program that prints the minimum number of coins to make change for an amount of money.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Prints the program name.
+* @argc: Number of commmand arguments
+* @argv: Array of strings of the size of argc
+* Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+
+int cents;
+int num = 0;
+
+if (argc ==  1)
+{
+printf("Error\n");
+return (1);
+}
+else if (argc > 1)
+{
+cents = atoi(argv[1]);
+if (cents >= 25)
+{
+num += cents / 25;
+cents %= 25;
+}
+if (cents >= 10)
+{
+num += cents / 10;
+cents %= 10;
+}
+if (cents >= 5)
+{
+num += cents / 5;
+cents %= 5;
+}
+if (cents >= 2)
+{
+num += cents / 2;
+cents %= 2;
+}
+if (cents >= 1)
+{
+num += cents / 1;
+cents %= 1;
+}
+printf("%d\n", num);
+}
+return (0);
+}
