2b01ac526627730e173417f1bcbe848bc4520c32
chrisSE2022 <mwanikichris90@gmail.com>
2022-07-22 04:48:49 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,8 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
-/*copy list from task*/
 int _putchar(char c);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
