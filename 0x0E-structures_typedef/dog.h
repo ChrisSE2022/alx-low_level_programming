6d832096974075901e78667651126f6fad884f9b
chrisSE2022 <mwanikichris90@gmail.com>
2022-08-01 11:15:25 -0700

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,34 +1,21 @@
 #ifndef _DOG_H
 #define _DOG_H
-
-#define NIL "(nil)"
-
 #include <stdio.h>
-
 /**
-* struct dog - structure for dogs
-* @name: first member
-* @age: second member
-* @owner: third member
+* struct dog - struct to define a dog
+* @name: string
+* @age: float
+* @owner: string
 */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-/**
-* dog_t - Typedef for struct dog
-*/
-
-typedef struct dog dog_t;
 #endif
