98f656580c6f8d380b087e0643b23edc4286597e
chrisSE2022 <mwanikichris90@gmail.com>
2022-08-11 08:26:26 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* premain - before the main function is executed..
+*
+* Return: ntg.
+*/
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
